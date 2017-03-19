3e6fc4bc8de39e7fc5427bee6363f56c250c4978
aumgn <aumgn@free.fr>
2017-03-20 00:32:36 +0100

    [Emacs] Tweaking ivy

--- emacs/.emacs.d/aumgn/ivy.el
+++ emacs/.emacs.d/aumgn/ivy.el
@@ -1,5 +1,12 @@
+(use-package flx)
+
 (use-package counsel
+  :init
+  (setq ivy-re-builders-alist
+	'((t . ivy--regex-fuzzy)))
   :config
   (ivy-mode 1)
-  (dim-minor-name 'ivy-mode ""))
+  (dim-minor-name 'ivy-mode "")
+  (define-key evil-normal-state-map "/" #'swiper)
+  (define-key aumgn/space-map "/" #'counsel-ag))
 
